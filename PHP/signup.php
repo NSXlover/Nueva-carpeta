2932ace6d32aae810e9d588116108e7417e86478
Rodrigo <rgtzrbl@gmail.com>
2024-01-26 04:59:31 +0100

    deploy en railway

--- PHP/signup.php
+++ PHP/signup.php
@@ -4,7 +4,7 @@ session_start();
 include './conexion.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['btnSignUp'])) {
 
         //Extract the pass
         $pass = $_POST['password'];
